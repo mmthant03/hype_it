e494a7e2797ab64ce9f9d79274d8dce4a7dbea73
Myo Min Thant <mmthant@wpi.edu>
2018-09-29 13:46:52 -0400

    Add database connection

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 let http = require('http'),
     fs = require('fs'),
     url = require('url'),
-    port = process.env.PORT || 8080,
-    pg = require('pg');
+    port = process.env.PORT || 8080;
 
+// connecting Database
+const { Pool } = require('pg');
+const connectionUrl = "postgres://mqjevsmzvtzfbm:4a94df64ee8fe5bc17e09d879441f16b8010ada026f6997b6fb29cf3b5e41471@ec2-50-16-196-57.compute-1.amazonaws.com:5432/d8218gqfsn80ii";
+const pool = new Pool({
+    connectionString: connectionUrl,
+    ssl: true,
+});
+pool.connect();
+
+// starting Server
 let server = http.createServer(function (req,res) {
     let uri = url.parse(req.url);
     console.log(req.method + " " + uri.pathname);
